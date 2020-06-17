733d47c2847e4fd1f6b9f552678f08beb6c92240
Rafael <arafael.menchaca@gmail.com>
2020-06-16 23:46:30 -0500

    Se modifico con removeEventListener

--- ejercicio_mouse/evento_mouse.js
+++ ejercicio_mouse/evento_mouse.js
@@ -1,18 +1,6 @@
-var mouse = {
-  UP: -1,
-  DOWN: 1,
-  LEFT: -1,
-  RIGHT: 1
-}
-
-document.getElementById("area_de_dibujo").addEventListener("mousemove", dibujar_mouse);
-document.getElementById("area_de_dibujo").addEventListener("mouseup", dibujar_mouse);
-document.getElementById("area_de_dibujo").addEventListener("mousedown", dibujar_mouse);
-var papel =  document.getElementById("area_de_dibujo").getContext("2d");
-var x = 150;
-var y = 150;
-
-dibujar_linea("red", x-1, y-1, x+1, y+1, papel);
+document.getElementById("area_de_dibujo").addEventListener("mousedown", activa_desactiva);
+var papel = document.getElementById("area_de_dibujo").getContext("2d");
+var estado = "desactivado";
 
 function dibujar_linea(color, x_inicial, y_inicial, x_final, y_final, lienzo){
   lienzo.beginPath();
@@ -24,32 +12,47 @@ function dibujar_linea(color, x_inicial, y_inicial, x_final, y_final, lienzo){
   lienzo.closePath();
 }
 
+function activa_desactiva(){
+  if (estado == "activado"){
+    document.getElementById("area_de_dibujo").addEventListener("mousemove", dibujar_mouse);
+    estado = "desactivado";
+  }
+  else if (estado == "desactivado"){
+    document.getElementById("area_de_dibujo").removeEventListener("mousemove", dibujar_mouse);
+    estado = "activado";
+  }
+}
+
 function dibujar_mouse(evento){
   console.log(evento);
   var movimiento = 1;
   var colorcito = "red";
+  var x = evento.layerX;
+  var y = evento.laverY;
 
-  switch (evento.movementY){
-    case mouse.UP:
-      dibujar_linea(colorcito, x, y, x, y - movimiento, papel);
-      y = y - movimiento;
-    break;
+  dibujar_linea(colorcito, x, y, x + 1, y + 1, papel);
 
-    case mouse.DOWN:
-      dibujar_linea(colorcito, x, y, x, y + movimiento, papel);
-      y = y + movimiento;
-    break;
-  }
-
-  switch (evento.movementX) {
-    case mouse.LEFT:
-      dibujar_linea(colorcito, x, y, x - movimiento, y, papel);
-      x = x - movimiento;
-    break;
-
-    case mouse.RIGHT:
-    dibujar_linea(colorcito, x, y, x + movimiento, y, papel);
-    x = x + movimiento;
-    break;
-  }
+  // switch (evento.movementY){
+  //   case mouse.UP:
+  //     dibujar_linea(colorcito, x, y, x, y - movimiento, papel);
+  //     y = y - movimiento;
+  //   break;
+  //
+  //   case mouse.DOWN:
+  //     dibujar_linea(colorcito, x, y, x, y + movimiento, papel);
+  //     y = y + movimiento;
+  //   break;
+  // }
+  //
+  // switch (evento.movementX) {
+  //   case mouse.LEFT:
+  //     dibujar_linea(colorcito, x, y, x - movimiento, y, papel);
+  //     x = x - movimiento;
+  //   break;
+  //
+  //   case mouse.RIGHT:
+  //   dibujar_linea(colorcito, x, y, x + movimiento, y, papel);
+  //   x = x + movimiento;
+  //   break;
+  // }
 }
